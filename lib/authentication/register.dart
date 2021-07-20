c8874879ccc373f044df99a9289e4fa9bad450d5
pavithrahareddy <pavithrahareddy@gmail.com>
2021-07-20 14:22:11 +0530

    connected firebase

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yeah/Theme/constants.dart';
+import 'package:sizer/sizer.dart';
+
 class RegisterScreen extends StatefulWidget {
   static const String id = 'register_screen';
 
@@ -10,11 +12,29 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          backgroundTheme,
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            backgroundTheme,
+            Container(
+              height: 80.h,
+              width: 80.w,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.3),
+                borderRadius: BorderRadius.all(Radius.circular(30))
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Morse \nChat",textAlign: TextAlign.center,style: TextStyle(color: Colors.white,fontSize: 30.sp),),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yeah/authentication/landing.dart';
 import 'package:yeah/authentication/login.dart';
 import 'package:yeah/authentication/register.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,14 +11,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: RegisterScreen.id,
-      routes: {
-        LoginScreen.id: (context) => LoginScreen(),
-        RegisterScreen.id: (context) => RegisterScreen(),
-        LandingScreen.id: (context) => LandingScreen(),
-      },
+    return Sizer(
+        builder: (context, orientation, deviceType){
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: RegisterScreen.id,
+            routes: {
+              LoginScreen.id: (context) => LoginScreen(),
+              RegisterScreen.id: (context) => RegisterScreen(),
+              LandingScreen.id: (context) => LandingScreen(),
+            },
+          );
+        }
     );
   }
 }
