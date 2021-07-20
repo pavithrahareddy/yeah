e17a847c0b2d23b7e598522fc93daf07c58f27a4
pavithrahareddy <pavithrahareddy@gmail.com>
2021-07-20 12:49:20 +0530

    Added Authentication Screens

---
+++ lib/authentication/landing.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  static const String id = 'landing_screen';
+
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
